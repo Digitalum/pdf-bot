ae2c6ffeb74f05a551747ca6489b01108bed0127
Digitalum <50397564+Digitalum@users.noreply.github.com>
2019-08-01 11:34:22 +0200

    Update htmlpost.js

--- src/storage/htmlpost.js
+++ src/storage/htmlpost.js
@@ -1,1 +1,63 @@
+var debug = require('debug')('pdf:s3')
+var s3 = require('s3')
+var path = require('path')
 
+function createHtmlPostStorage(options = {}) {
+  if (!options.accessKeyId) {
+    throw new Error('S3: No access key given')
+  }
+
+
+  return function uploadToS3 (localPath, job) {
+    
+    return new Promise((resolve, reject) => {
+      var client = s3.createClient(
+          Object.assign(options.s3ClientOptions || {},
+          {
+            s3Options: {
+              accessKeyId: options.accessKeyId,
+              secretAccessKey: options.secretAccessKey,
+              region: options.region,
+            }
+          }
+        )
+      )
+
+      var remotePath = (options.path || '')
+      if (typeof options.path === 'function') {
+        remotePath = options.path(localPath, job)
+      }
+
+      var pathSplitted = localPath.split('/')
+      var fileName = pathSplitted[pathSplitted.length - 1]
+      var fullRemotePath = path.join(remotePath, fileName)
+
+      var uploadOptions = {
+        localFile: localPath,
+
+        s3Params: {
+          Bucket: options.bucket,
+          Key: fullRemotePath,
+        },
+      }
+
+      debug('Pushing job ID %s to S3 path: %s/%s', job.id, options.bucket, fileName)
+
+      var uploader = client.uploadFile(uploadOptions);
+      uploader.on('error', function(err) {
+        reject(err)
+      });
+      uploader.on('end', function(data) {
+        resolve({
+          path: {
+            bucket: uploadOptions.s3Params.Bucket,
+            region: options.region,
+            key: uploadOptions.s3Params.Key
+          }
+        })
+      });
+    })
+  }
+}
+
+module.exports = createHtmlPostStorage
